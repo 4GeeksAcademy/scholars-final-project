9a0dbe59b4a808536157f2bc1fee4a467fbdfdd0
Adbeel Rodriguez <adbeelrodriguez99@gmail.com>
2024-12-20 03:09:17 +0000

    update

--- src/front/js/pages/dashboard.jsx
+++ src/front/js/pages/dashboard.jsx
@@ -1,62 +1,41 @@
 import React from "react";
-import NavGroup from "../component/navGroup.jsx";
-import CardsCourses from "../component/CardsCourses.jsx";
+import CardsCourses from "../component/dashboard components/CardsCourses.jsx";
+import Calendar from "../component/dashboard components/Calendar.jsx";
+import Grades from "../component/dashboard components/Grades.jsx";
+import Notebook from "../component/dashboard components/Notebooks.jsx";
+import Assignments from "../component/dashboard components/Assignments.jsx";
+import VerticalMenu from "../component/dashboard components/VerticalMenu.jsx";
+import { useEffect } from "react";
+import { useState } from "react";
 
-const DashBoard = () =>{
-    let isCourseActive = true;
-    let isCalendarActive = false;
-    let isAssignmentsActive = false;
-    let isGradesActive = false;
-    let isNotebookActive = false;
-
-    const HandleShowPage = () =>{
-        
-    }
-
+const DashBoard = ({courseActive,calendarActive,assigmentsActive,gradesActive,notebookActive}) =>{
+    const [changeContent,setChangeContent]=useState(false);
     return(
         <>
             <div className="Dashboard-container d-flex">
                 <div className="text-center ms-3 mt-3 ps-3 pe-3">
-                    <div className="list-group">
-                        <a href="#" className="list-group-item list-group-item-action" onClick={() => HandleShowPage}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-house-door-fill mt-2" viewBox="0 0 16 16">
-                                <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/>
-                            </svg>
-                            <p className="ms-1 mt-1">All Courses</p>
-                        </a>
-                        <a href="#" className="list-group-item list-group-item-action">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-person-video3 mt-1" viewBox="0 0 16 16">
-                                <path d="M14 9.5a2 2 0 1 1-4 0 2 2 0 0 1 4 0m-6 5.7c0 .8.8.8.8.8h6.4s.8 0 .8-.8-.8-3.2-4-3.2-4 2.4-4 3.2"/>
-                                <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h5.243c.122-.326.295-.668.526-1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v7.81c.353.23.656.496.91.783Q16 12.312 16 12V4a2 2 0 0 0-2-2z"/>
-                            </svg>
-                            <p className="ms-1">Grades</p>
-                        </a>
-                        <a href="#" className="list-group-item list-group-item-action">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-calendar-week-fill mt-1" viewBox="0 0 16 16">
-                                <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2M9.5 7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5m3 0h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5M2 10.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5"/>
-                            </svg>
-                            <p className="ms-1">Calendar</p>
-                        </a>
-                        <a href="#" className="list-group-item list-group-item-action">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-journal-bookmark-fill mt-1" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M6 1h6v7a.5.5 0 0 1-.757.429L9 7.083 6.757 8.43A.5.5 0 0 1 6 8z"/>
-                                <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2"/>
-                                <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z"/>
-                            </svg>
-                            <p className="ms-1">Notebook</p>
-                        </a>
-                        <a href="#" className="list-group-item list-group-item-action">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-newspaper mt-1" viewBox="0 0 16 16">
-                                <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5z"/>
-                                <path d="M2 3h10v2H2zm0 3h4v3H2zm0 4h4v1H2zm0 2h4v1H2zm5-6h2v1H7zm3 0h2v1h-2zM7 8h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2z"/>
-                            </svg>
-                            <p className="ms-1">Assignments</p>
-                        </a>
-                    </div>
+                    <VerticalMenu />
                 </div>
 
                 <div className="ms-5">
-                    <CardsCourses/>
+                    {useEffect(()=>{
+                        if(courseActive == null && calendarActive == null && assigmentsActive == null && gradesActive == null && notebookActive === null)
+                            return(<CardsCourses/>)
+                        if(changeContent)
+                            setChangeContent(false);
+                        setChangeContent(true);
+                        console.log(courseActive);
+                        if(courseActive)
+                            return(<CardsCourses/>)
+                        if(calendarActive)
+                            return(<Calendar/>)
+                        if(assigmentsActive)
+                            return(<Assignments/>)
+                        if(gradesActive)
+                            return(<Grades/>)
+                        if(notebookActive)
+                            return(<Notebook/>)
+                    },[calendarActive,courseActive,assigmentsActive,gradesActive,notebookActive])}
                 </div>
             </div>
         </>
