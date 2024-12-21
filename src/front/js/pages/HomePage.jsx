3a45da06a673858511ec55b02216145d214e89ff
Kacian417 <knicho4492@gmail.com>
2024-12-21 00:21:24 +0000

    home page

--- src/front/js/pages/HomePage.jsx
+++ src/front/js/pages/HomePage.jsx
@@ -5,57 +5,79 @@ const HomePage = () => {
     return (
         <>
         
-        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-label="Slide 1" aria-current="true"></button>
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class=""></button>
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
+        <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-indicators">
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="active" aria-label="Slide 1" aria-current="true"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" className=""></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" className=""></button>
             </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="https://fastly.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I" class="img-fluid" alt="..."/>
-                    <div class="container">
-                    <div class="carousel-caption text-start">
-                        <h1><strong>Welcome to Scholars.ai!</strong></h1>
-                        <p>Some representative placeholder content for the first slide of the carousel.</p>
-                        <p><a class="btn btn-lg btn-primary" href="#">Learn More</a></p>
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                <div className="card mb-3 style={max-width=540px}">
+                <div className="row g-0">
+                    <div className="col-md-4">
+                    <img src="https://fastly.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I" className="img-fluid rounded-start" alt="..."/>
                     </div>
-                    </div>
-                </div>
-
-                <div class="carousel-item">
-                    <img src="https://picsum.photos/2000/800" class="img-fluid" alt="..."/>
-                    <div class="container">
-                    <div class="carousel-caption">
-                        <h1><strong>Get Started Today!</strong></h1>
-                        <p>Some representative placeholder content for the second slide of the carousel.</p>
-                        <p><a class="btn btn-lg btn-primary" href="#">Sign Up</a></p>
-                    </div>
-                    </div>
-                </div>
-
-                <div class="carousel-item">
-                <img src="https://picsum.photos/2000/800" class="img-fluid" alt="..."/>
-                    <div class="container">
-                    <div class="carousel-caption text-end">
-                        <h1>One more for good measure.</h1>
-                        <p>Some representative placeholder content for the third slide of this carousel.</p>
-                        <p><a class="btn btn-lg btn-primary" href="#">Browse Courses</a></p>
-                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title"><strong>Welcome to Scholars.ai!</strong></h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
                     </div>
                 </div>
             </div>
+                </div>
 
-            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
+                <div className="carousel-item">
+                    <img src="https://cdn.pixabay.com/photo/2020/10/26/19/44/e-learning-5688627_960_720.png" className="img-fluid" alt="..."/>
+                    <div className="container">
+                        <div className="carousel-caption">
+                            <h1><strong>Get Started Today!</strong></h1>
+                            <p>Some representative placeholder content for the second slide of the carousel.</p>
+                            <p><a className="btn btn-lg btn-primary" href="#">Sign Up</a></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="carousel-item">
+                <img src="https://picsum.photos/2000/800" className="img-fluid" alt="..."/>
+                    <div className="container">
+                        <div className="carousel-caption text-end">
+                            <h1>One more for good measure.</h1>
+                            <p>Some representative placeholder content for the third slide of this carousel.</p>
+                            <p><a className="btn btn-lg btn-primary" href="#">Browse Courses</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
+            <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
             </button>
         </div>
         This the home page
+        <div className="row">
+            <div className="card mb-3 style={max-width=540px}">
+                <div className="row g-0">
+                    <div className="col-md-4">
+                    <img src="..." className="img-fluid rounded-start" alt="..."/>
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         
         </>
     );
